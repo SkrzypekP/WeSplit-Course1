aa15b0d3080cc15d89a1b35979014d4c04f7a007
pskrzypek <91827235+SkrzypekP@users.noreply.github.com>
2022-04-28 20:09:30 +0200

    Initial Commit

---
+++ Course-WeSplitApp/Course_WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_WeSplitAppApp.swift
+//  Course-WeSplitApp
+//
+//  Created by Patryk on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Course_WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
