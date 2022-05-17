46ae45df44519de28e5ba3528773b3732b5acc11
pskrzypek <91827235+SkrzypekP@users.noreply.github.com>
2022-05-17 17:55:16 +0200

    WeSplit - final App

--- Course-WeSplitApp/ContentView.swift
+++ Course-WeSplitApp/ContentView.swift
@@ -9,26 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let clients = ["Januszex", "Mirex", "Polex"]
-    @State private var selectedClient = "Januszex"
+    @State private var ammount: Double = 0
+    @State private var people = 0
+    @State private var tipPercentage = 20
+    
+    @FocusState private var ammountIsFocused: Bool
+    
+    let tipPercTable = [0, 10, 20, 25]
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(people) + 2
+        let money = (Double(ammount))*(Double(tipPercentage)/100) + Double(ammount)
+            return money/peopleCount
+    }
     
     var body: some View {
-        NavigationView{
-            Form{
-                Picker("Select your Client", selection: $selectedClient) {
-                    ForEach(clients, id: \.self){
-                        Text($0)
+        NavigationView {
+                Form {
+                    Section {
+                        TextField("Ammount: ", value: $ammount, format: .currency(code: Locale.current.currencyCode ?? "PLN"))
+                                .keyboardType(.decimalPad)
+                                .focused($ammountIsFocused)
+                            
+                        Picker("Number of people", selection: $people){
+                            ForEach(2..<21) {
+                                Text("\($0) people")
+                            }
+                        }
+                        
+                        
+                    }
+                    
+                    Section {
+                        Picker("Tip percentage", selection: $tipPercentage){
+                            ForEach(tipPercTable, id: \.self){
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("How much tip do you wany to leave")
+                    }
+                    
+                    Section {
+                        Text("\(totalPerPerson) \(Locale.current.currencyCode ?? "PLN") per person")
+                    }
+                    
+                .navigationTitle("We split")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Spacer ()
+                        Button("Done") {
+                            ammountIsFocused = false
+                        }
                     }
                 }
-            }
-            .navigationTitle("Clients database")
-            .navigationBarTitleDisplayMode(.inline)
+                }
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
 }
